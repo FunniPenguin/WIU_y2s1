87fea4b4227e624740b74892cbba3c4d301cf853
FunniPenguin <yapdylan845@gmail.com>
2025-08-25 17:46:36 +0800

    Quests

--- WIU-y2s1/Assets/Scripts/MonoBehaviour/Dylan/QuestSystem/QuestData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class QuestData
-{
-    bool _completionStatus;
-    string _name;
-
-}
---
+++ WIU-y2s1/Assets/Scripts/MonoScripts/Dylan/QuestData.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "QuestData", menuName = "QuestData/QuestData")]
+public class QuestData : ScriptableObject
+{
+    private bool _completionStatus = false;
+    [SerializeField] private string _name = "";
+    [SerializeField] private string _description = "";
+    private int _objectiveProgress = 0;
+    [SerializeField] private int _completionCount = 1;
+
+    private void Awake()
+    {
+        _objectiveProgress = 0;
+        _completionStatus = false;
+    }
+    public bool GetCompletionStatus()
+    {
+        return _completionStatus;
+    }
+    public string GetName()
+    {
+        return _name;
+    }
+    public string GetDescription()
+    {
+        return _description;
+    }
+    public int GetObjectiveProgress()
+    {
+        return _objectiveProgress;
+    }
+    public int GetCompletionCount() { 
+        return _completionCount;
+    }
+    public void SetCompletionStatus(bool Status) { 
+        _completionStatus = Status;
+    }
+    public void UpdateObjectiveCount(int Count)
+    {
+        _objectiveProgress += Count;
+        if (_objectiveProgress > _completionCount)
+        {
+            _objectiveProgress = _completionCount;
+        }
+    }
+}
