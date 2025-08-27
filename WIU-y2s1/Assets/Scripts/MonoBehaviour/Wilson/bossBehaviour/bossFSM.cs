69775da16bfa06ef478265a4e2cdcdf5d4de91d3
WR540 <wilsoncheng889@gmail.com>
2025-08-27 17:51:56 +0800

    First 1.5 phases of boss

--- WIU-y2s1/Assets/Scripts/MonoBehaviour/Wilson/bossBehaviour/bossFSM.cs
+++ WIU-y2s1/Assets/Scripts/MonoBehaviour/Wilson/bossBehaviour/bossFSM.cs
@@ -8,6 +8,8 @@ public class bossFSM : MonoBehaviour
     private GameObject boss;
     private Rigidbody2D rb; //boss Rigidbody2D.
 
+    [SerializeField] private GameObject projectilePrefab;
+
     private CircleCollider2D attackCollider;
 
     private Vector2 toPlayer = Vector2.zero;
@@ -16,13 +18,13 @@ public class bossFSM : MonoBehaviour
     private float bossSpeed = 0f;
     private float bossJump = 0f;
 
-    //phase 1 variables start
+    //phase 1.1 variables start
 
     private const float perimeterDistance = 6f;
-    private float subStateTransitionTimer = 3f;
+    private float subStateTransitionTimer = 5f;
 
-    //phase 1 variables end
-    //phase 2 variables start
+    //phase 1.1 variables end
+    //phase 1.2 variables start
 
     private int attackSubStateAmount = 0;
     private enum attackSubStateEnum
@@ -34,7 +36,18 @@ public class bossFSM : MonoBehaviour
     private attackSubStateEnum attackSubState = attackSubStateEnum.charge;
     private float attackingTimer = 0.25f;
 
-    //phase 2 variables end
+    //phase 1.2 variables end
+    //phase 2.1 variables start
+
+    private float bombardPrimeTimer = 0.5f;
+
+    private float projectileVel = 10f;
+    private float gravity = 10f;
+    private float firePower = 10f;
+
+    private float aim = 0f;
+
+    //phase 2.1 variables end
 
     private Vector2 movementTarget = Vector2.zero;
 
@@ -47,7 +60,9 @@ public class bossFSM : MonoBehaviour
     private enum subStateEnum
     {
         evade1,
-        attack1
+        attack1,
+        prime2,
+        bombard2
     }
     private subStateEnum subState = subStateEnum.evade1;
 
@@ -62,12 +77,15 @@ public class bossFSM : MonoBehaviour
     private void Start()
     {
         health = 100f;
-        bossSpeed = 5f * Time.deltaTime;
+        bossSpeed = 3f * Time.deltaTime;
         bossJump = 10f;
         boss = GameObject.FindGameObjectWithTag("Boss");
         player = GameObject.FindGameObjectWithTag("Player");
         rb = boss.GetComponent<Rigidbody2D>();
         attackCollider = boss.GetComponent<CircleCollider2D>();
+
+        phaseState = phaseStateEnum.phaseTwo;
+        subState = subStateEnum.prime2;
     }
 
     private void Update()
@@ -75,83 +93,129 @@ public class bossFSM : MonoBehaviour
         //Debug.Log(attackSubStateAmount);
 
         toPlayer = player.transform.position - boss.transform.position;
-
-        if (subState == subStateEnum.evade1)
+        if (phaseState == phaseStateEnum.phaseOne)
         {
-            if (subStateTransitionTimer <= 0)
+            if (subState == subStateEnum.evade1)
             {
-                subState = subStateEnum.attack1;
-                attackSubStateAmount = 5;
-                subStateTransitionTimer = 3f;
-            }
+                if (subStateTransitionTimer <= 0)
+                {
+                    subState = subStateEnum.attack1;
+                    attackSubStateAmount = 5;
+                    subStateTransitionTimer = 5f;
+                }
 
-            movementTarget = new Vector2(player.transform.position.x + (isPerimeterLEFT ? -perimeterDistance : perimeterDistance), player.transform.position.y);
-            RaycastHit2D rightFlipValidator = Physics2D.Raycast(boss.transform.position, new Vector2(3f, -1f), 3.1f, LayerMask.GetMask("Ground"));
-            RaycastHit2D leftFlipValidator = Physics2D.Raycast(boss.transform.position, new Vector2(-3f, -1f), 3.1f, LayerMask.GetMask("Ground"));
-            if (rightFlipValidator.collider != null && !isPerimeterLEFT)
-            {
-                isPerimeterLEFT = true;
-            }
-            if (leftFlipValidator.collider != null && isPerimeterLEFT)
-            {
-                isPerimeterLEFT = false;
-            }
+                movementTarget = new Vector2(player.transform.position.x + (isPerimeterLEFT ? -perimeterDistance : perimeterDistance), player.transform.position.y);
+                RaycastHit2D rightFlipValidator = Physics2D.Raycast(boss.transform.position, new Vector2(3f, -1f), 3.1f, LayerMask.GetMask("Ground"));
+                RaycastHit2D leftFlipValidator = Physics2D.Raycast(boss.transform.position, new Vector2(-3f, -1f), 3.1f, LayerMask.GetMask("Ground"));
+                if (rightFlipValidator.collider != null && !isPerimeterLEFT)
+                {
+                    isPerimeterLEFT = true;
+                }
+                if (leftFlipValidator.collider != null && isPerimeterLEFT)
+                {
+                    isPerimeterLEFT = false;
+                }
 
-            if (toPlayer.magnitude < 2.5f)
-            {
-                _JUMP = true;
-            }
-
-            subStateTransitionTimer -= Time.deltaTime;
-
-
-            //Debug.DrawLine(boss.transform.position, new Vector2(boss.transform.position.x + 3, boss.transform.position.y - 1));
-            //Debug.DrawLine(boss.transform.position, new Vector2(boss.transform.position.x - 3, boss.transform.position.y - 1));
-        }
-        else if (subState == subStateEnum.attack1)
-        {
-            if (attackSubState == attackSubStateEnum.charge)
-            {
-                movementTarget = player.transform.position;
                 if (toPlayer.magnitude < 2.5f)
                 {
-                    attackSubState = attackSubStateEnum.attack;
-                    _JUMP = Random.Range(0, 1) == 0;
+                    _JUMP = true;
                 }
+
+                subStateTransitionTimer -= Time.deltaTime;
+
+
+                //Debug.DrawLine(boss.transform.position, new Vector2(boss.transform.position.x + 3, boss.transform.position.y - 1));
+                //Debug.DrawLine(boss.transform.position, new Vector2(boss.transform.position.x - 3, boss.transform.position.y - 1));
             }
-            else if (attackSubState == attackSubStateEnum.attack)
+            else if (subState == subStateEnum.attack1)
             {
-                attackCollider.enabled = true;
-                attackingTimer -= Time.deltaTime;
-                if (attackingTimer <= 0)
+                if (attackSubState == attackSubStateEnum.charge)
                 {
-                    attackingTimer = 0.25f;
-                    isPerimeterLEFT = !isPerimeterLEFT;
-                    attackSubState = attackSubStateEnum.recuperate;
-                }
-            }
-            else if (attackSubState == attackSubStateEnum.recuperate)
-            {
-                attackCollider.enabled = false;
-                movementTarget = new Vector2(player.transform.position.x + (isPerimeterLEFT ? -perimeterDistance : perimeterDistance), player.transform.position.y);
-                _JUMP = true;
-                if (toPlayer.magnitude > 6f)
-                {
-                    attackSubState = attackSubStateEnum.charge;
-                    if (attackSubStateAmount <= 0)
+                    movementTarget = player.transform.position;
+                    if (toPlayer.magnitude < 2.5f)
                     {
-                        subState = subStateEnum.evade1;
-                        attackSubStateAmount = 5;
-                        subStateTransitionTimer = 3f;
+                        attackSubState = attackSubStateEnum.attack;
+                        _JUMP = Random.Range(0, 1) == 0;
                     }
-                    else
+                }
+                else if (attackSubState == attackSubStateEnum.attack)
+                {
+                    attackCollider.enabled = true;
+                    attackingTimer -= Time.deltaTime;
+                    if (attackingTimer <= 0)
                     {
-                        attackSubStateAmount -= 1;
+                        attackingTimer = 0.25f;
+                        isPerimeterLEFT = !isPerimeterLEFT;
+                        attackSubState = attackSubStateEnum.recuperate;
+                    }
+                }
+                else if (attackSubState == attackSubStateEnum.recuperate)
+                {
+                    attackCollider.enabled = false;
+                    movementTarget = new Vector2(player.transform.position.x + (isPerimeterLEFT ? -perimeterDistance : perimeterDistance), player.transform.position.y);
+                    _JUMP = true;
+                    if (toPlayer.magnitude > 6f)
+                    {
+                        attackSubState = attackSubStateEnum.charge;
+                        if (attackSubStateAmount <= 0)
+                        {
+                            subState = subStateEnum.evade1;
+                            attackSubStateAmount = 5;
+                            subStateTransitionTimer = 3f;
+                        }
+                        else
+                        {
+                            attackSubStateAmount -= 1;
+                        }
                     }
                 }
             }
         }
+        else if (phaseState == phaseStateEnum.phaseTwo)
+        {
+            if (subState == subStateEnum.prime2)
+            {
+                movementTarget = (Vector2)(transform.position) - toPlayer;
+                bombardPrimeTimer -= Time.deltaTime;
 
+                if (bombardPrimeTimer <= 0)
+                {
+                    bombardPrimeTimer = 0.5f;
+                    subState = subStateEnum.bombard2;
+                }
+            }
+            else if (subState == subStateEnum.bombard2)
+            {
+                var projectile = Instantiate(projectilePrefab);
+                projectile.transform.position = transform.position;
+                int validAngles = 0;
+                var discriminant = (Mathf.Pow(projectileVel, 4)) - (gravity * ((gravity * toPlayer.x * toPlayer.x) + (2 * (toPlayer.y * projectileVel * projectileVel))));
+                if (discriminant < 0)
+                {
+                    validAngles = 0;
+                }
+                else if (discriminant == 0)
+                {
+                    validAngles = 1;
+                }
+                else
+                { validAngles = 2; }
+                float sqrtDisc = Mathf.Sqrt(discriminant);
+                float angle1 = Mathf.Atan((projectileVel * projectileVel + sqrtDisc) / (gravity * toPlayer.x));
+                float angle2 = Mathf.Atan((projectileVel * projectileVel - sqrtDisc) / (gravity * toPlayer.x));
+                RaycastHit2D hitResult = Physics2D.Raycast(transform.position, toPlayer, 3, LayerMask.GetMask("Ground"));
+                float[] validAngleArray = new float[] { angle1, angle2 };
+                aim = validAngleArray.Length == 1 ? validAngleArray[0] : (hitResult.collider != null ? validAngleArray[0] : validAngleArray[1]);
+                Vector2 launchDir = new Vector2(Mathf.Cos(aim), Mathf.Sin(aim));
+                launchDir *= Mathf.Sign(toPlayer.x);
+                if (validAngles == 0)
+                {
+                    launchDir = new Vector2(toPlayer.x >= 0f ? .75f : -.75f, .75f);
+                }
+                projectile.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(launchDir.x * firePower, launchDir.y * firePower);
+                projectile.GetComponent<Rigidbody2D>().angularVelocity = -(projectile.GetComponent<Rigidbody2D>().linearVelocityX) * 100f;
+            }
+        }
         _LEFT = movementTarget.x < boss.transform.position.x;
         _RIGHT = movementTarget.x > boss.transform.position.x;
 
