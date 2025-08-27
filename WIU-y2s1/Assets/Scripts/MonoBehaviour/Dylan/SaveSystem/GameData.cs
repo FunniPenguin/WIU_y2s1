af2c2f019c418e74c0b126e712108a296eed114a
FunniPenguin <yapdylan845@gmail.com>
2025-08-27 14:27:13 +0800

    fixed the save sys

--- WIU-y2s1/Assets/Scripts/MonoBehaviour/Dylan/SaveSystem/GameData.cs
+++ WIU-y2s1/Assets/Scripts/MonoBehaviour/Dylan/SaveSystem/GameData.cs
@@ -8,21 +8,25 @@ using _Inventory.Model;
 [System.Serializable]
 public class GameData
 {
-    int _currentLevel;
+    public int _currentLevel; 
+    public string _activeQuestGUID;
 
     //Do not save player position as player should spawn at player spawn point
 
     public SerializableDictionary<string, bool> mapGameObjects; //string is the guid, bool is whether the object is active
-    public List<SerializableInventoryItem> inventoryItems; //string is the item name, int is the quantity
-    public SerializableDictionary<string, bool> savePoints; //string is the id, bool is whether to spawn the player
+    //public List<InventoryItem> inventoryItems; //string is the item name, int is the quantity
+    //public SerializableDictionary<string, bool> savePoints; //string is the id, bool is whether to spawn the player
+    public SerializableDictionary<string, int> questData;
     public GameData()
     {
         _currentLevel = 1;
+        _activeQuestGUID = "";
         //_playerPosition = new Vector3(0, 0, 0);
         //_currMapIndex = GameSceneManager.Instance.GetStartingLevelIndex();
         mapGameObjects = new SerializableDictionary<string, bool>();
-        inventoryItems = new List<SerializableInventoryItem>();
-        savePoints = new SerializableDictionary<string, bool>();
+        //inventoryItems = new List<InventoryItem>();
+        //savePoints = new SerializableDictionary<string, bool>();
+        questData = new SerializableDictionary<string, int>();
     }
 }
 
